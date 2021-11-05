eb1e8849155fc1986c54df4d456b542d973c19af
NatiTsegaye <86118205+NatiTsegaye@users.noreply.github.com>
2021-11-05 19:15:15 +0400

    Update __init__.py

--- Main/__init__.py
+++ Main/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 import os
 from Main.Routes.routes import initialize_routes
 from config import Config
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,9 +12,6 @@ api = Api(app)
 app.config['MONGODB_SETTINGS'] = {
     'host': Config.DB
 }
-
-CORS(app, resources={r'/*':{"origins": "*"}},allow_headers=[
-    "Content-Type", "Authorization", "Access-Control-Allow-Methods"])
 
 from Main.DAC.config import initialize_db
 initialize_db(app)
